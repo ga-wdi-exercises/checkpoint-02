1f42479ecb4f0f29c1031e4e7ab335043281175a
Tim Foley <thetimfoley@gmail.com>
2016-09-19 09:16:02 -0400

    added event listener and basic bulb color changing

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,10 @@
+//create variable for all bulbs
 var $bulb = $('.bulb');
+
+//register event listener
+$bulb.on('click', function() {
+  //remove active class from all bulbs
+  $bulb.removeClass('active');
+  //add active class to the bulb we clicked on
+  $(this).addClass('active');
+})
