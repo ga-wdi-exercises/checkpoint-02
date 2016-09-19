7990c266952046eb134e40dd4b3b21e1cfa832a9
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-09-19 10:20:53 -0400

    facial_hair added

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 // Create an array called `colors` that contains three colors.
 var colors = ["blue","red","yellow"]
+
 // Log the last item in the array to the console.
 console.log (colors[0])
+
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 for (var i = 0; i < colors.length; i++) {
   console.log ("My favorite color is" + " " + (colors[0]));
@@ -11,11 +13,12 @@ for (var i = 0; i < colors.length; i++) {
 // Create an `instructor` object that contains three key-value pairs.
 var instructor =
 {
-  gender:"Fiat",
+  gender:"Male",
   mindset:"500",
   swag:"On-point"
 };
-
 // Add a `facial-hair` property to `instructor`.
+instructor.facial_hair = "Beard";
 
 // Log the value of the `facial-hair` property to the console.
+console.log(instructor, [0]);
