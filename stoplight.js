1dbf5964e467bcaf340a5858fdc1029748f8bb32
Ian Hollands <ihollands@outlook.com>
2016-09-19 09:34:47 -0400

    Javascript Part II: 3, 4

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,20 @@
-console.log("I work!")
+var redBulb = $("#red");
+var yellowBulb = $("#yellow");
+var greenBulb = $("#green");
+
+activateRed = function() {
+  redBulb.toggleClass("active")
+}
+
+activateYellow = function() {
+  yellowBulb.toggleClass("active")
+}
+
+activateGreen = function() {
+  greenBulb.toggleClass("active")
+}
+
+
+redBulb.on("click", activateRed)
+yellowBulb.on("click", activateYellow)
+greenBulb.on("click", activateGreen)
