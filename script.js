ad549c528016ec264de3897898273372f3622f61
Andrew Whitley <awhitley1233@gmail.com>
2016-09-19 09:08:14 -0400

    added console.log for last color

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Create an array called `colors` that contains three colors.
 colors = ['red', 'black', 'white'];
 // Log the last item in the array to the console.
-
+console.log(colors[colors.length-1]);
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+// for (i = 0; i < colors.length; i++) {
+//   console.log(colors[i]);
+// };
 // Create an `instructor` object that contains three key-value pairs.
 
 // Add a `facial-hair` property to `instructor`.
