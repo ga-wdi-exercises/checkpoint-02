1f9c1382fdedab9d1ff856f5b9da5b52be19cc41
Michael Lai <mike24567@gmail.com>
2016-09-19 09:49:06 -0400

    created click listener to turn on a light

---
+++ stoplight.js
@@ -0,0 +1,14 @@
+//Turns top red
+$("#red").on("click", function() {
+    $(this).css("background", "red");
+});
+
+//Turns middle yellow
+$("#yellow").on("click", function() {
+    $(this).css("background", "yellow");
+});
+
+//Turns bottom green
+$("#green").on("click", function() {
+    $(this).css("background", "green");
+});
