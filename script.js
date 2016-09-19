72ea13b5210950c209208877a73237a9211da31f
Andrew Whitley <awhitley1233@gmail.com>
2016-09-19 09:08:36 -0400

    added for loop to print out all colors in array

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ colors = ['red', 'black', 'white'];
 // Log the last item in the array to the console.
 console.log(colors[colors.length-1]);
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-// for (i = 0; i < colors.length; i++) {
-//   console.log(colors[i]);
-// };
+for (i = 0; i < colors.length; i++) {
+  console.log(colors[i]);
+};
 // Create an `instructor` object that contains three key-value pairs.
 
 // Add a `facial-hair` property to `instructor`.
