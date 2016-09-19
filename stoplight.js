d614bd7227d604fa9b49c53d63c1886fe8f670e6
Mark Sweet <marks828@gmail.com>
2016-09-19 09:53:22 -0400

    switch active class

--- stoplight.js
+++ stoplight.js
@@ -1,3 +1,18 @@
 $('#stoplight').children().on('click', function(){
-  $(this).toggleClass('active')
+  $(this).toggleClass('active');
+  $('#red').on('click', function(){
+    $(this).toggleClass('active')
+    $('#yellow').removeClass('active')
+    $('#green').removeClass('active')
+  })
+  $('#yellow').on('click', function(){
+    $(this).toggleClass('active')
+    $('#red').removeClass('active')
+    $('#green').removeClass('active')
+  })
+  $('#green').on('click', function(){
+    $(this).toggleClass('active')
+    $('#red').removeClass('active')
+    $('#yellow').removeClass('active')
+  })
 })
