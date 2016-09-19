2c7afbd0c69e98a6ed10e42ea27a836c1216f63b
Andrew Whitley <awhitley1233@gmail.com>
2016-09-19 09:54:14 -0400

    only one light can activate at a time (still trying to reset yellow each iteration)

--- stoplight.js
+++ stoplight.js
@@ -1,9 +1,11 @@
 $('div.bulb').on('click', function() {
     var bulb = this;
+    $('div.bulb').removeClass('active');
+    $('#red').eq(1).attr('id', 'yellow');
     $(bulb).toggleClass('active');
     if ($(bulb).attr('id') == 'yellow') {
       setTimeout (function() {
-        $(bulb).css('background', 'red');
+        $(bulb).attr('id', 'red');
       }, 2000);
     };
 });
