8ab9a4fb8c33923053fa0b13c8476a7809735b49
Poh Chuan Kuan <pohckuan@yahoo.com>
2016-09-19 09:35:09 -0400

    created event listeners for three lights

---
+++ stoplight.js
@@ -0,0 +1,12 @@
+$("#red").on("click",changeToRed);
+  function changeToRed() {
+   $("#red").css("background","red");
+  }
+$("#yellow").on("click",changeToYellow);
+    function changeToYellow() {
+     $("#yellow").css("background","yellow");
+    }
+$("#green").on("click",changeToGreen);
+      function changeToGreen() {
+       $("#green").css("background","green");
+      }
