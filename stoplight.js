729bf95cb8015ad8f23e11697c4c5b2e24c65903
Domonique Griffin <griffind2009@gmail.com>
2016-09-19 09:53:04 -0400

    variables created

---
+++ stoplight.js
@@ -0,0 +1,11 @@
+var redLight = $("#red");
+var redLight =button.on("click", function changeBackgroundColorToRed(){
+})
+
+var yellowLight = $("#yellow");
+var yellowLight = button.on("click", function changeBackgroundColorToYellow(){
+})
+
+var greenLight = $("#green");
+var greenLight = button.on("click", function changeBackgroundColorToGreen(){
+})
