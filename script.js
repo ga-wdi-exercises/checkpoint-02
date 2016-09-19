e13cce93caa922589f6fa42c6af56618bb7b8215
brown2206 <brown.angelo.jr@gmail.com>
2016-09-19 09:07:33 -0400

    updated index and script

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create an array called `colors` that contains three colors.
-
+var colors = ['red', 'white', 'orange'];
 // Log the last item in the array to the console.
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
