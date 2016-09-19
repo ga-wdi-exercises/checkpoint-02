ae0ff67f9de82afad2d2ee4e3ed8cbe3f35938c7
Chris Jaffier <chrisd@Chriss-MacBook-Pro.local>
2016-09-19 09:48:53 -0400

    print out favorite colors

--- script.js
+++ script.js
@@ -3,8 +3,13 @@ var colors = ["blue","red","yellow"]
 // Log the last item in the array to the console.
 console.log (colors[0])
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+for (var i = 0; i < colors.length; i++) {
+  console.log ("My favorite color is" + " " + (colors[0]));
+  console.log ("My favorite color is" + " " + (colors[1]));
+  console.log ("My favorite color is" + " " + (colors[2]));
+}
 // Create an `instructor` object that contains three key-value pairs.
+
 
 // Add a `facial-hair` property to `instructor`.
 
