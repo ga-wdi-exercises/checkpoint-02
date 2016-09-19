5938d96a4faecfe576ed0c419c2bab21317edce3
Domonique Griffin <griffind2009@gmail.com>
2016-09-19 09:56:52 -0400

    changed background colors

--- stoplight.js
+++ stoplight.js
@@ -1,4 +1,4 @@
-var redLight = $("#red");
+/*var redLight = $("#red");
 var redLight =button.on("click", function changeBackgroundColorToRed(){
 })
 
@@ -8,4 +8,23 @@ var yellowLight = button.on("click", function changeBackgroundColorToYellow(){
 
 var greenLight = $("#green");
 var greenLight = button.on("click", function changeBackgroundColorToGreen(){
-})
+})*/
+
+$(function() {
+$('#red').click(function(){
+    $(this).css('background', 'red');
+
+  });
+});
+$(function() {
+$('#yellow').click(function(){
+    $(this).css('background', 'yellow');
+
+  });
+});
+$(function() {
+$('#green').click(function(){
+    $(this).css('background', 'green');
+
+  });
+});
