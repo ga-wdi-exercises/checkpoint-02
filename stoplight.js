b18a71ea28048d35a9d88d8699fff0d5d00dffa0
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-19 09:58:01 -0400

    stopligt

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,9 @@
 console.log("hi");
+
+// var button = $("#stoplight");
+// function handlClickEvent() {
+//   console.log("I was Clicked")
+// }
+document.getElementById('red').onclick = illuminateRed;
+document.getElementById('yellow').onclick = illuminateYellow;
+document.getElementById('green').onclick = illuminateGreen;
