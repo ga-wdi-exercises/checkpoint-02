37b585b3325da5d484c1957864be7020e61b6617
Latoya Watson <latoyamwatson@gmail.com>
2016-09-19 09:40:59 -0400

    added functions and click events for lights

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,24 @@
-alert("I'm working");
+var yellow = $("#stoplight #yellow");
+var green = $("#stoplight #green");
+var red = $("#stoplight #red");
+
+// Yellow Light
+function handleYellow() {
+  yellow.css("background-color", "yellow");
+}
+
+yellow.on("click", handleYellow);
+
+// Green Light
+function handleGreen() {
+  green.css("background-color", "green");
+}
+
+green.on("click", handleGreen);
+
+// Red Light
+function handleRed() {
+  red.css("background-color", "red");
+}
+
+red.on("click", handleRed);
