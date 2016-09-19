6d29065f235e729f2589848b697e39fff9d2c965
Barrett Cope <barrettcope@gmail.com>
2016-09-19 09:06:42 -0400

    Create array

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create an array called `colors` that contains three colors.
-
+var colors = ['red','blue','yellow'];
 // Log the last item in the array to the console.
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
