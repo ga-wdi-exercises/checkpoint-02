43378362c2c42d14752eef6315945f106338cbfc
Andrew Whitley <awhitley1233@gmail.com>
2016-09-19 09:11:07 -0400

    added instructor object

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Create an array called `colors` that contains three colors.
-colors = ['red', 'black', 'white'];
+var colors = ['red', 'black', 'white'];
 // Log the last item in the array to the console.
 console.log(colors[colors.length-1]);
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
@@ -7,7 +7,11 @@ for (i = 0; i < colors.length; i++) {
   console.log(colors[i]);
 };
 // Create an `instructor` object that contains three key-value pairs.
-
+var instructor = {
+  name : 'Adrian',
+  height : "6'",
+  hairColor : 'brown'
+}
 // Add a `facial-hair` property to `instructor`.
 
 // Log the value of the `facial-hair` property to the console.
