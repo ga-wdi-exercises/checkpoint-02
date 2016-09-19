743d2e2ba324352e9c6582764ed751bdc25f6def
Michael Rubin <michael@rubinsworld.com>
2016-09-19 09:59:18 -0400

    completed checkpoint 2 ran out of time

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,18 @@
-console.log("stoplight.js Loaded")
+console.log("stoplight.js Loaded");
+var redLight = $("#red");
+var yellowLight = $("#yellow");
+var greenLight = $("#green");
+
+redLight.on("click", turnRedOn);
+yellowLight.on("click", turnYellowOn);
+greenLight.on("click",turnGreenOn);
+
+function turnGreenOn() {
+  console.log("GREEN LIGHT!");
+}
+function turnRedOn (){
+  console.log("RED LIGHT!");
+}
+function turnYellowOn (){
+  console.log("YELLOW LIGHT!");
+}
