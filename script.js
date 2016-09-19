4c39e3955d4c53b646f99905e6ee89571953c9d2
RYANBROPHY <BROPHY2321@ME.COM>
2016-09-19 09:16:48 -0400

    log

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 
 // Create an array called `colors` that contains three colors.
+var colors = ['red', 'green', 'blue'];
 
 // Log the last item in the array to the console.
-
+colors[2];
+console.log("blue");
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 
 // Create an `instructor` object that contains three key-value pairs.
