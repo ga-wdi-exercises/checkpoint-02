be0ee345f4ad18f75067b6f6b67f0e84bbe9dd40
Alexander <ajspottswood@gmail.com>
2016-09-20 00:38:19 -0400

    PartIIquestion5

--- stoplight.js
+++ stoplight.js
@@ -1,4 +1,9 @@
 $(".bulb").on("click", function() {
   $(this).toggleClass("active");
-}
-);
+  if ($(this).attr("id") == "yellow") {
+    setTimeout (function() {
+      $("#red").toggleClass("active");
+      $("#yellow").removeClass("active");
+    }, 2000);
+  }
+});
