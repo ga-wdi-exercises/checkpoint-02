ee176bcb8f5f20bee5990d05bfff3c35a1e89fba
Alexander <ajspottswood@gmail.com>
2016-09-19 23:25:32 -0400

    question2

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Create an array called `colors` that contains three colors.
 colors = {"red", "green", "blue"}
 // Log the last item in the array to the console.
-
+for (i = 0; i < colors.length; i++) {
+    console.log(colors{i})
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 
 // Create an `instructor` object that contains three key-value pairs.
