e5e68efd11d74991e962f98bd8be43744613044f
Sam Catherman <Sam.Catherman@gmail.com>
2016-10-19 09:31:23 -0400

    part two q3

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,12 @@
-console.log("stoplight says hello");
+// console.log("stoplight says hello");
+$(document).ready(function(){
+  $("#red").on("click", function(){
+    $("#red").css("background-color", "red")
+  })
+  $("#yellow").on("click", function(){
+    $("#yellow").css("background-color", "yellow")
+  })
+  $("#green").on("click", function(){
+    $("#green").css("background-color", "green")
+  })
+})
