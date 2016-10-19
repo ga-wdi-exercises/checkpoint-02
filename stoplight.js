6af4961bd2f701204b962c0fa6a17a67c94947a1
EJ Sorrento <ejsorrento@gmail.com>
2016-10-19 10:02:42 -0400

    Commit quiz EJ

---
+++ stoplight.js
@@ -0,0 +1,31 @@
+//For some reason my "spotlight.js" wouldn't link
+//to the page, tried changing the tag and their position
+//please halp!
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+});
+if ($("#red").on(click, function())) {
+  $("#red").addClass("active");
+  $("#red").removeClass("active");
+}
+else
+{$("#red").addClass("active")};
+});
+
+if ($("#yellow").on(click, function())) {
+  $("#yellow").addClass("active");
+  $("#yellow").removeClass("active");
+}
+else {
+  $("#yellow").addClass("active");
+}
+});
+
+if ($("#green").on(click, function())) {
+  $("#green").addClass("active");
+  $("#green").removeClass("active");
+  else {
+    $("#green").addClass("active");
+  }
+});
