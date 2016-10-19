6109db3e5d39c333abcefd1ce5520fcd546c7aa8
Kevin Herrera <1herrerake@gmail.com>
2016-10-19 09:55:52 -0400

    question 9

--- stoplight.js
+++ stoplight.js
@@ -6,6 +6,13 @@ spotlight>#red.on("click", changeBackgroundColorToRed);
 spotlight>#yellow.on("click", changeBackgroundColorToYellow);
 spotlight>#green.on("click", changeBackgroundColorToGreen);
 
+spotlight>#red.toggle();
+spotlight>#yellow.toggle();
+spotlight>#green.toggle();
+
+
+
+
 
 }
 
