8c94add10817acd2636cf37d5bd21efb9de1cdac
Kevin Herrera <1herrerake@gmail.com>
2016-10-19 09:53:19 -0400

    guestion 8

---
+++ stoplight.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+
+var spotlight = $("#stoplight");
+
+spotlight>#red.on("click", changeBackgroundColorToRed);
+spotlight>#yellow.on("click", changeBackgroundColorToYellow);
+spotlight>#green.on("click", changeBackgroundColorToGreen);
+
+
+}
+
+})
