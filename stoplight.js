948cc2b37db217a395937b3b3642a3adc5f3bd38
Thinh Nguyen <nguyendthinh@outlook.com>
2016-10-19 09:34:35 -0400

    answer question 4 of part 2

--- stoplight.js
+++ stoplight.js
@@ -1,3 +1,11 @@
 $(".bulb").on("click", function() {
+
+var onOrNot= $(this).attr("class");
+console.log(onOrNot);
+
+if (onOrNot === "bulb") {
   $(this).addClass("active");
+} else {
+  $(this).removeClass("active");
+}
 })
