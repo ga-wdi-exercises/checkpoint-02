fc943c69bf12487e5a287f693ddfb2560313dfb6
Sam Mans <smans123@gmail.com>
2016-10-19 10:01:02 -0400

    Got to question 4

--- stoplight.js
+++ stoplight.js
@@ -4,24 +4,24 @@ function assignRed () {
   $("#red").css({"background":"red"});
     $("#red").on("click", assignBlack);
       function assignBlack () {
-        $("#red").css({"background":"#555"});
+      $("#red").css({"background":"#555"});
       }
 }
 
 $("#yellow").on("click", assignYellow);
-function assignYellow () {
-  $("#yellow").css({"background":"yellow"});
-    $("#yellow").on("click", assignBlack);
-      function assignBlack () {
+  function assignYellow () {
+    $("#yellow").css({"background":"yellow"});
+      $("#yellow").on("click", assignBlack);
+        function assignBlack () {
         $("#yellow").css({"background":"#555"});
     }
 }
 
 $("#green").on("click", assignGreen);
-function assignGreen () {
-  $("#green").css({"background":"green"});
-    $("#green").on("click", assignBlack);
-      function assignBlack () {
+  function assignGreen () {
+    $("#green").css({"background":"green"});
+      $("#green").on("click", assignBlack);
+        function assignBlack () {
         $("#green").css({"background":"#555"});
     }
 }
