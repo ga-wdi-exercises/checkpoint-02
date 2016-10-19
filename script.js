7e8450cd69722d689ab9c0333761c80ca4f491e5
Sam Mans <smans123@gmail.com>
2016-10-19 09:38:24 -0400

    added even listeners

--- script.js
+++ script.js
@@ -5,9 +5,15 @@ $(document).ready(function() {
 })
 // Create an array called `colors` that contains three colors.
 
+var colors = ["green", "yellow", "red"];
+
 // Log the last item in the array to the console.
+console.log(colors[2])
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+for (i = 0; i < colors.length; i++) {
+  console.log(colors[i])
+}
 
 // Create an `instructor` object that contains three key-value pairs.
 
--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,15 @@
-console.log("hello")
+// 3. Create an event listener(s) so that when you click on a light, it turns to its respective color (i.e., top light turns red).
+$("#red").on("click", assignRed);
+function assignRed () {
+  $("#red").css({"background":"red"});
+}
+
+$("#yellow").on("click", assignYellow);
+function assignYellow () {
+  $("#yellow").css({"background":"yellow"});
+}
+
+$("#green").on("click", assignGreen);
+function assignGreen () {
+  $("#green").css({"background":"green"});
+}
