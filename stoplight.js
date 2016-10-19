6b0c74e70cab062d3d94d1d031e9688d8526b346
robert <robert.k25@gmail.com>
2016-10-19 10:03:58 -0400

    refactor

--- stoplight.js
+++ stoplight.js
@@ -1,26 +1,17 @@
 
 $(document).ready(function () {
-  $("#red").on("click", function(evt) {
-    $("#green").removeClass("active");
-    $("#yellow").removeClass("active");
-    $(this).toggleClass("active");
-  });
-
-  $("#yellow").on("click", function(evt) {
-    $("#yellow").removeClass("active");
-    $("#red").removeClass("active");
-    $(this).toggleClass("active");
-    setTimeout(function () {
-      $("#red").addClass("active");
-      $("#yellow").removeClass("active");
-    }, 2000);
-
-  });
-
-  $("#green").on("click", function(evt) {
-    $(this).toggleClass("active");
-    $("#yellow").removeClass("active");
-    $("#red").removeClass("active");
-  });
+  $(".bulb").on("click", toggleLight);
 
 });
+
+
+function toggleLight(evt) {
+  $(this).siblings().removeClass("active");
+  $(this).toggleClass("active");
+  if ($(this).attr("id")  == "yellow") {
+    setTimeout(function(){
+      $("#red").addClass("active");
+      $("#red").siblings().removeClass("active");
+    }, 2000);
+  }
+}
