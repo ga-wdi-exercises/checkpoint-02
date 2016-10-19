8e47c36e41c16ae559e1b536c7af98fe6f8a2148
Jonathan Reiser <jonathanreiser@Jonathans-MacBook-Pro-2.local>
2016-10-19 09:33:52 -0400

    turns off active if already active onclick

--- stoplight.js
+++ stoplight.js
@@ -1,6 +1,10 @@
 $(document).ready(function(){
   $('.bulb').on("click", turnOn);
   function turnOn(){
+    if ($(this).hasClass('active')){
+      $(this).removeClass('active');
+    } else {
     $(this).addClass('active');
+  }
   }
 })
