cd2a8c322e8a888b3fb48d5382abfb8de564a203
Bradley Cravens <bradleycravens@gmail.com>
2016-10-19 09:27:35 -0400

    Part 1 question four and five

--- script.js
+++ script.js
@@ -7,7 +7,12 @@ for (i=0;i<colors.length;i++) {
   console.log("My favorite color is " + colors[i]);
 }
 // Create an `instructor` object that contains three key-value pairs.
-
+var instructor = {
+  nayana: "first",
+  andy: "second",
+  james: "third"
+};
 // Add a `facial-hair` property to `instructor`.
-
+instructor["facial-hair"]= "a lot";
 // Log the value of the `facial-hair` property to the console.
+console.log(instructor['facial-hair']);
