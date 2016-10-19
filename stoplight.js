e491deb1f0dc980d42198eb15edcc30a03f7cbd1
Jonathan Reiser <jonathanreiser@Jonathans-MacBook-Pro-2.local>
2016-10-19 10:28:01 -0400

    final draft

--- stoplight.js
+++ stoplight.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
   $('.bulb').on("click", turnOn);
   function turnOn(){
+    var self = $(this);
     if ($(this).hasClass('active')){
       $(this).removeClass('active');
     } else {
     $(this).addClass('active');
+    if (this.id === "yellow"){
+      setTimeout(myFunction, 2000);
+      function myFunction(){
+        self.attr('id', 'red');
+        console.log("hello");
+      }
+    }
   }
   }
 })
