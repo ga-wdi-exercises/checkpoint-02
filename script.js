f0e982d709fda3590e60b497173c66f8b163cd8c
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-01-30 09:48:34 -0500

    I_need_help_with_event_listeners

--- script.js
+++ script.js
@@ -9,7 +9,15 @@ for(var i=0; i<10, i++){
   'my favorite color is red';
 }
 // Create an `instructor` object that contains three key-value pairs.
-
+var hotel = {
+  Name: 'Hilton',
+  Rooms: '50',
+  Booked: '50',
+  facial-hair: '1'
+}
 // Add a `facial-hair` property to `instructor`.
 
 // Log the value of the `facial-hair` property to the console.
+
+facial-hair:'1'
+console.log
