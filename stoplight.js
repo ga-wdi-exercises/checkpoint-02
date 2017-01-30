2daa2bdd7e57bb8e726695d7045757f52661987e
Erica Norby <ericanorby@gmail.com>
2017-01-30 09:49:31 -0500

    only one bulb can be active at a time

--- stoplight.js
+++ stoplight.js
@@ -8,17 +8,20 @@ function changeLight(){
   }
   else if ($(this).attr("id") == "red") {
     $("#red").addClass("active")
+    $("#green,#yellow").removeClass("active")
   }
   else if ($(this).attr("id") == "yellow"){
     $("#yellow").addClass("active")
+    $("#red,#green").removeClass("active")
     setTimeout(yellowToRed, 2000)
   }
   else if ($(this).attr("id") == "green"){
     $("#green").addClass("active")
+    $("#yellow,#red").removeClass("active")
   }
 }
 
 function yellowToRed(){
-  $("#yellow").removeClass("active")
+  $("#yellow,#green").removeClass("active")
   $("#red").addClass("active")
 }
