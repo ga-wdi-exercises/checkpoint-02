00e82ec7138699be62c4a81e887a2d4074e4b69d
Bill Bobbitt <wmbobbitt@gmail.com>
2017-01-30 09:26:40 -0500

    part 2 5 and 6 done

--- stoplight.js
+++ stoplight.js
@@ -4,6 +4,15 @@ $('.bulb').on('click', function() {
   if ($(this).hasClass('active')) {
     $(this).removeClass('active')
   } else {
-  $(this).addClass('active');
+    $('.bulb').removeClass('active');
+    $(this).addClass('active');
   }
+})
+
+$('#yellow').on('click', function() {
+  $('#yellow').addClass('active');
+  setTimeout(function() {
+    $('#yellow').removeClass('active');
+    $('#red').addClass('active');
+  }, 2000)
 })
