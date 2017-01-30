eb1434767fa812d41dce49cb00757c1078198309
Nyketha Lyken <Nyketha.Lyken@gmail.com>
2017-01-30 09:05:30 -0500

    created an array with three colors

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Create an array called `colors` that contains three colors.
 
+var colors = ['red', 'yellow', 'green']; 
 // Log the last item in the array to the console.
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
