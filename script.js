8098a1dca9fc7e5827ad64ef4f13d2610a51ee1d
ISAAC BENJAMIN DUARTE <Isaac.b.duarte@icloud.com>
2017-01-30 09:44:29 -0500

    done

--- script.js
+++ script.js
@@ -15,3 +15,38 @@ var instructor = {
 instructor['facial-hair'] = 'rad';
 
 console.log(instructor['facial-hair']);
+
+$('#red').on('click',function() {
+  var self = $(this).attr('class');
+  if (self.includes('active') == true) {
+    $(this).removeClass('active');
+  }
+  else {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  }
+})
+$('#yellow').on('click',function() {
+  var self = $(this).attr('class');
+  if (self.includes('active') == true) {
+    $(this).removeClass('active');
+  }
+  else {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+    setTimeout(function(){
+      $('#yellow').removeClass('active');
+      $('#yellow').siblings('#red').addClass('active');
+    }, 2000);
+  }
+})
+$('#green').on('click',function() {
+  var self = $(this).attr('class');
+  if (self.includes('active') == true) {
+    $(this).removeClass('active');
+  }
+  else {
+    $(this).addClass('active');
+    $(this).siblings().removeClass('active');
+  }
+})
