a3ff8aed808910db9a6f4166b75caabede2183a0
Savraj Singh <savrajsingh@gmail.com>
2017-01-30 09:57:42 -0500

    more

--- stoplight.js
+++ stoplight.js
@@ -5,21 +5,36 @@ var clearLight = $("#stoplight div")
 
 //Clear stoplight
 
-var changeLights = function(){
-  if($("#stoplight").css("background-color") != "black"){
-    clearLight.on("click").css({"background-color": "black"});
+var changeRed = function(){
+  if($("#stoplight #red").css("background-color") != "black"){
+    pressOnRed.on("click").css({"background-color": "black"});
   }
 
-  else if (pressOnRed.css("background-color") = "black"){
+  else{
     pressOnRed.on("click").css({"background-color": "red"})
   }
-  else if (pressOnYellow.css("background-color") = "black"){
-    pressOnYellow.on("click").css({"background-color": "yellow"})
-  }
-  else if(pressOnGreen.css("background-color") = "black"){
-    pressOnGreen.on("click").css({"background-color": "green"})
-  }
-
 };
 
-changeLights();
+changeRed();
+
+var changeYellow = function(){
+  if($("#stoplight #yellow").css("background-color") != "black"){
+    pressOnYellow.on("click").css({"background-color": "black"});
+  }
+else{
+  pressOnYellow.on("click").css({"background-color": "yellow"})
+}
+}
+changeYellow();
+
+
+var changeGreen = function(){
+  if($("#stoplight #green").css("background-color") != "black"){
+    pressOnGreen.on("click").css({"background-color": "black"});
+  }
+else{
+  pressOnGreen.on("click").css({"background-color": "green"})
+  }
+}
+
+changeGreen();
