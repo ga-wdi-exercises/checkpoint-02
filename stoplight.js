dea247c55ee35313a696eb6dbc319571f685daa1
Erica Norby <ericanorby@gmail.com>
2017-01-30 09:26:43 -0500

    created event listeners to turn on lights

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,15 @@
-console.log("JS is working");
+$("#red").click(turnOn)
+$("#yellow").click(turnOn)
+$("#green").click(turnOn)
+
+function turnOn(){
+  if ($(this).attr("id") == "red") {
+    $("#red").addClass("active")
+  }
+  if ($(this).attr("id") == "yellow"){
+    $("#yellow").addClass("active")
+  }
+  if ($(this).attr("id") == "green"){
+    $("#green").addClass("active")
+  }
+}
