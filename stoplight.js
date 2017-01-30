d29b94daba23edbd3b13a9b5d0206d92d4fe29d4
Andy Flickinger <atflick@gmail.com>
2017-01-30 09:50:07 -0500

    js2 q5 and q6 done

--- stoplight.js
+++ stoplight.js
@@ -1,7 +1,23 @@
+// Q3 and Q4
 $(".bulb").click(function(){
   if($(this).hasClass("active")) {
     $(this).removeClass("active")
   } else {
+    $(".bulb").removeClass("active")
     $(this).addClass("active")
   }
 })
+
+// Q5
+
+
+$("#yellow").click(
+    function() {
+        $("#yellow").addClass("active");
+        setTimeout(
+            function() {
+              $("#yellow").removeClass("active");
+              $("#red").addClass("active");
+            },
+            2000);
+    });
