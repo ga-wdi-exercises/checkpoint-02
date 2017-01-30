cb22e75358e1e6b4508c826bb62c0c55717c61d3
Justin Shin <jjs3b@virginia.edu>
2017-01-29 21:15:28 -0500

    6 of JS02, made it so clicking inactive light activates light and deactivates any other light. caught bug where clicking yellow and then green immediately will end up with both green and red lit up.  Also cancel timer for lighting red if you deactivate yellow when it is still lit.

--- stoplight.js
+++ stoplight.js
@@ -1,4 +1,11 @@
+var myTimer
 $('.bulb').on('click',function(){
+  if(myTimer != undefined){
+    clearTimeout(myTimer)
+  }
+  if(!($(this).is('.active')) && ($(this).siblings().is('.active'))){
+    $(this).siblings('.active').removeClass('active')
+  }
   switch ($(this).is('.active')){
     case true:
       $(this).removeClass('active');
@@ -7,8 +14,8 @@ $('.bulb').on('click',function(){
       $(this).addClass('active');
       break;
   }
-  if($(this).is('#yellow')){
-    var myTimer = setTimeout(function(){
+  if($(this).is('#yellow')&&$(this).is('.active')){
+    myTimer = setTimeout(function(){
       $('#yellow').removeClass('active')
       $('#red').addClass('active')
     },2000)
