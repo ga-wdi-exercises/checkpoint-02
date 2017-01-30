fb2767d95cb614d265ab4fbc238f32c04bae089f
Eva Lai <evalai.dc@gmail.com>
2017-01-30 09:22:18 -0500

    click to add color to light

---
+++ stoplight.js
@@ -0,0 +1,17 @@
+// event listeners to when click on light, it turns to its respective color
+$('#red').on('click',function () {
+  $('#red').addClass('active');
+})
+$('#yellow').on('click',function () {
+  $('#yellow').addClass('active');
+})
+$('#green').on('click',function () {
+  $('#green').addClass('active');
+})
+// click a light that is on/ turns it off
+// $('#red').on('click',function () {
+//   $('#red').removeClass('active');
+// })
+//  click yellow, and it switches to red after 2 seconds
+
+//make it so that only one light can be turned on at a time.
