d2cf690b4f039c96eb636b40be3a323ae61e9af5
Lisa Anne Burr <burr_lisa@bah.com>
2017-01-30 09:24:51 -0500

    completed JS pt 1

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 // Create an array called `colors` that contains three colors.
 
+var color = ["blue", "red", "yellow"];
+
 // Log the last item in the array to the console.
+
+console.log(color [2]);
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 
+for (var i = 0; i < color.length; i++) {
+  console.log("My favorite color is " + color[i] + ".");
+}
+
 // Create an `instructor` object that contains three key-value pairs.
+
+var instructor = {
+  firstName: "Bob",
+  lastName: "Smith",
+  eyeColor: "blue"
+};
 
 // Add a `facial-hair` property to `instructor`.
 
+instructor.facialHair = "scruffy";
+
 // Log the value of the `facial-hair` property to the console.
+
+console.log(instructor.facialHair);
