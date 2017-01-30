9ca4722330714e68914dd91c419bdfcbaabbfb91
Amilcar Sylvester <THE_EMAIL_YOU_USE_FOR_GITHUB@EMAIL.COM>
2017-01-30 09:22:07 -0500

    part1

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 // Create an array called `colors` that contains three colors.
-
+  var colors = [ “red, ‘blue’, ‘green’];
 // Log the last item in the array to the console.
+alert(colors[3]);
+console.log
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+for(var i=0; i<10, i++){
+  'my favorite color is red';
+}
 // Create an `instructor` object that contains three key-value pairs.
 
 // Add a `facial-hair` property to `instructor`.
