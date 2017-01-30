e5900ff798ccae6ea3fef0f195e9cd0d481f36f6
Justin Shin <jjs3b@virginia.edu>
2017-01-29 20:47:00 -0500

    4 of JS03, added logic if light is on, turn off if off turn on

--- stoplight.js
+++ stoplight.js
@@ -1,3 +1,10 @@
 $('.bulb').on('click',function(){
-  $(this).addClass('active');
+  switch ($(this).is('.active')){
+    case true:
+      $(this).removeClass('active');
+      break;
+    case false:
+      $(this).addClass('active');
+      break;
+  }
 })
