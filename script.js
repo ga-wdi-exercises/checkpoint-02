ccd0fb4eea521db4f4eee769f3fb0f97e600c7c1
Ebenezer Williams <ebenezerw@me.com>
2017-01-30 09:35:32 -0500

    for loop

--- script.js
+++ script.js
@@ -11,6 +11,9 @@ console.log(colors[2]);
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 
+for (var i = 0; i < 3; i++) {
+   console.log("My favorite color is" + " " + colors);
+}
 // Create an `instructor` object that contains three key-value pairs.
 
 // Add a `facial-hair` property to `instructor`.
