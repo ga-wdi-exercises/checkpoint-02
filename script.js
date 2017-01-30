d5aa66ae56ea3e472c76ff1f9d7425a423ce73dd
KEVIN KELLER <keller_kevin@bah.com>
2017-01-30 09:50:17 -0500

    final

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
 // Create an array called `colors` that contains three colors.
-
+colors = ["green","yellow","red"];
 // Log the last item in the array to the console.
-
+console.log(colors[2]);
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+for (var i=0; i < colors.length; i++) {
+  console.log("My favorite color is " + colors[i]);
+}
 // Create an `instructor` object that contains three key-value pairs.
-
+instructor = {
+  yearsTeaching : 4,
+  collegeDegree : 'yes',
+  hair : 'bald'
+}
 // Add a `facial-hair` property to `instructor`.
-
+instructor.facialHair = 'beard'
 // Log the value of the `facial-hair` property to the console.
+console.log(instructor.facialHair);
