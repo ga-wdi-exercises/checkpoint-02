c7b1c6c160b90410dfaf103a96bb3ab7e714a22f
Mitchell Carroll <jmitchellcarroll@gmail.com>
2017-01-30 09:56:30 -0500

    checkpoint-02

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-// Create an array called `colors` that contains three colors.
-
-// Log the last item in the array to the console.
-
+var colors = ['red','yellow','green']
+console.log(colors.slice(-1)[0]);
+for(colors = 0; colors < 3; colors ++) {
+  console.log ("My favorite color is" + 'colors' )
+}
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 
 // Create an `instructor` object that contains three key-value pairs.
-
+var instructor = {
+  language: "English"
+  school: "General Assembly"
+  experience: "development"
+}
 // Add a `facial-hair` property to `instructor`.
-
+instructor.facialhair: "beard"
 // Log the value of the `facial-hair` property to the console.
+console.log(facialhair)
