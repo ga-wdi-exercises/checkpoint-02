a52a2125709b6892cf7c0be3d0902eef3267be69
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-30 09:40:45 -0500

    loged the value of the facial-hair property

--- script.js
+++ script.js
@@ -25,8 +25,8 @@ var instructor = function (Adrian){
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`. (x)
 
-// Create an `instructor` object that contains three key-value pairs.
+// Create an `instructor` object that contains three key-value pairs.(x)
 
-// Add a `facial-hair` property to `instructor`.
+// Add a `facial-hair` property to `instructor`. (x)
 
-// Log the value of the `facial-hair` property to the console.
+// Log the value of the `facial-hair` property to the console. (x)
