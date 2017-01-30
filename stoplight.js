d7db5e2b9e4b3dc2a3a891e3b720bb1dbf356d97
Chris Wightman <wightman_christopher@bah.com>
2017-01-30 09:51:39 -0500

    Commit Part II. I suck.

---
+++ stoplight.js
@@ -0,0 +1,7 @@
+// $(document).ready(function(){
+//  	$("body").css("background", "blue");
+// });
+
+$("#red").on("click").addClass("active#red");
+$("#yellow").on("click").addClass("active#yellow");
+$("#green").on("click").addClass("active#green");
