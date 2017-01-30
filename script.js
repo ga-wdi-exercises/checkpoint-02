217208bf67634f02402443a4dc00eb063615eefe
Bedig Galladian <bgalladian@gmail.com>
2017-01-30 09:49:50 -0500

    couldn't get lights to turn off

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
 // Create an array called `colors` that contains three colors.
+var colors = ["red", "blue", "orange"];
 
 // Log the last item in the array to the console.
+console.log(colors[2])
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+for (i = 0; i <= 2; i++) {
+  alert("My favoirte color is " + colors[i]);
+}
 
 // Create an `instructor` object that contains three key-value pairs.
+var insructor ={
+  name = "Matt"
+  height = "7 foot 10 inches",
+  age = "30"
+}
 
 // Add a `facial-hair` property to `instructor`.
+insructor["facial-hair"] = red;
 
 // Log the value of the `facial-hair` property to the console.
+consoloe.log(facial-hair);
 
-console.log("Hellow world");
+;
