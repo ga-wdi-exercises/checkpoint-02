4368c332e0f55f39ea3cd01295d45b4f3d10b71c
RODNEY CURL <RODNEY.CURL@GMAIL.COM>
2017-01-30 09:40:07 -0500

    made all of the required updates

--- script.js
+++ script.js
@@ -3,12 +3,27 @@ var colors = [
     "yellow",
     "green"
 ];
+console.log("green")
 
-// Create an array called `colors` that contains three colors.
+for (var colors = 0; colors < 3; colors ++)
+  console.log("My favorite color is red")
+  console.log("My favorite color is yellow")
+  console.log("My favorite color is green")
 
-// Log the last item in the array to the console.
+var instructor = function (Adrian){
+  instructor.name = Adrian,
+  instructor.school = GeneralAssembly,
+  instructor.class = WebDevelopmentImmersive,
+  instructor.facialhair = Beard
+  console.log(facialhair)
+};
 
-// Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+
+// Create an array called `colors` that contains three colors. (x)
+
+// Log the last item in the array to the console. (x)
+
+// Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`. (x)
 
 // Create an `instructor` object that contains three key-value pairs.
 
