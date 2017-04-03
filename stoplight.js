9848cf0ddac5912461d4672ae725bb2e9f57ae5e
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-03 09:44:53 -0400

    make it so both lights cannot be on at the same time

--- stoplight.js
+++ stoplight.js
@@ -2,10 +2,14 @@ $(document).ready(function () {
   //Red Light
   $("#red").on('click', function(){
     $("#red").toggleClass('active');
+    $("#yellow").removeClass('active');
+    clearInterval();
   })
   //Yellow light
   $("#yellow").on('click', function(){
     $("#yellow").toggleClass('active');
+    $("#red").removeClass('active')
+    //Switch to Red after 2 seconds
     setInterval(function(){
       $("#red").addClass('active');
       $("#yellow").removeClass('active');
