3855014d3044c006953abec5a293d88c6e3f773a
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-03 09:30:11 -0400

    logged array item to console

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Create an array called `colors` that contains three colors.
+var colors = {'green', 'yellow', 'red'};
 
 // Log the last item in the array to the console.
+
+   console.log(colors[2]); 
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
 
