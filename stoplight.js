ce2e3d09a109112e9161f7822cf66ad2df02a1f5
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-03 10:06:39 -0400

    attempt to simplify jquery and to clear interval after the light switches to red

--- stoplight.js
+++ stoplight.js
@@ -1,19 +1,17 @@
-$(document).ready(function () {
+$(document).ready(function() {
   //Red Light
   $("#red").on('click', function(){
-    $("#red").toggleClass('active');
+    $(this).toggleClass('active');
     $("#yellow").removeClass('active');
-    clearInterval();
-  })
+  });
   //Yellow light
   $("#yellow").on('click', function(){
-    $("#yellow").toggleClass('active');
+    $(this).toggleClass('active');
     $("#red").removeClass('active')
     //Switch to Red after 2 seconds
-    setInterval(function(){
+    var switchRed = setInterval(function(){
       $("#red").addClass('active');
       $("#yellow").removeClass('active');
     }, 2000);
-  })
-
+  });
 });
