81ff28430113ee2dbb2e10fce0797f2604c0ad1b
Curtis Ostle <curtisostle@gmail.com>
2017-04-03 09:53:01 -0400

    settimeout yellow to red

--- stoplight.js
+++ stoplight.js
@@ -2,6 +2,27 @@ var redlight = $('#red')
 var yellowlight = $('#yellow');
 var greenlight = $('#green');
 
-redlight.on('click',() => {redlight.toggleClass('active')});
-yellowlight.on('click',() => {yellowlight.toggleClass('active')});
-greenlight.on('click',() => {greenlight.toggleClass('active')});
+turnRed();
+
+redlight.on('click', () => {redlight.toggleClass('active')});
+yellowlight.on('click', turnYellow);
+greenlight.on('click',turnGreen);
+
+function turnGreen() {
+  greenlight.toggleClass('active');
+}
+
+function turnYellow() {
+  yellowlight.toggleClass('active');
+  setTimeout(yellowToRed,2000);
+}
+
+
+function yellowToRed() {
+  yellowlight.toggleClass('active');
+  turnRed();
+}
+
+function turnRed() {
+  redlight.toggleClass('active');
+}
