15758280578658a5ed779fcf03d91b714f554689
Whitney <whitney.purdum@gmail.com>
2017-04-03 09:21:44 -0400

    Add for-loop

--- script.js
+++ script.js
@@ -7,6 +7,11 @@ var colors = ['red', 'green', 'yellow']
 console.log(colors[2]);
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+for (var i = 0; i < colors.length; i++) {
+	console.log("My favorite color is [i]");
+};
+
+
 
 // Create an `instructor` object that contains three key-value pairs.
 
