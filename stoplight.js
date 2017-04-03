ed4c282b65368a66421730bdfddcd3f5df7e27e8
Adam Hoff <swimmerdude79@comcast.net>
2017-04-03 10:00:06 -0400

    Changed to setTimeout to get 1 light on at a time

--- stoplight.js
+++ stoplight.js
@@ -3,7 +3,6 @@ let $yellow = $("#yellow");
 let $green = $("#green");
 
 $red.on('click', function() {
-  clearInterval($yellow);
   $red.toggleClass("active");
   $yellow.removeClass("active");
   $green.removeClass("active");
@@ -12,13 +11,13 @@ $yellow.on('click', function() {
   $red.removeClass("active");
   $green.removeClass("active");
   $yellow.toggleClass("active");
-  setInterval(function () {
+  var myInterval = setTimeout(function () {
     $red.addClass("active");
     $yellow.removeClass("active");
   }, 2000);
+
 })
 $green.on('click', function() {
-  clearInterval($yellow);
   $green.toggleClass("active");
   $red.removeClass("active");
   $yellow.removeClass("active");
