d92f47403d5e0c45fefca37429c94bb52513ec37
Whitney <whitney.purdum@gmail.com>
2017-04-03 09:53:00 -0400

    create click events on stoplight - click light to change color; no light can have more than one color lit at a time

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
+$(document).ready(function(){
+
 // Create an array called `colors` that contains three colors.
+
+
 
 var colors = ['red', 'green', 'yellow'];
 var instructor = {
@@ -27,3 +31,45 @@ for (var i = 0; i < colors.length; i++) {
 instructor.facialhair = "none";
 
 // Log the value of the `facial-hair` property to the console.
+
+
+$("#red").click(function() {
+	$("#red").addClass("active");
+	$("#yellow").removeClass("active");
+	$("#green").removeClass("active");
+	});
+
+
+
+$("#green").click(function() {
+	$("#green").addClass("active");
+	$("#yellow").removeClass("active");
+	$("#red").removeClass("active");
+	});
+
+
+$("#yellow").click(function() {
+	$("#yellow").addClass("active");
+	$("#green").removeClass("active");
+	$("#red").removeClass("active");
+	});
+
+
+
+});
+
+
+
+
+
+
+
+
+
+/*1. Create an event listener(s) so that when you click on a light, it turns to its respective color (i.e., top light turns red). Examine `style.css` first and examine how the red light is being styled. 
+
+1. If you click a light that is already on, it turns off.
+
+1. If you click yellow, it switches to red after two seconds.
+
+1. Make it so that only one light can be turned on at a time (e.g., both red and yellow cannot be on at the same time). */
