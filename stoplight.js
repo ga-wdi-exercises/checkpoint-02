23047421ef1aad28761766f687ac7a3babc38f41
Adam Hoff <swimmerdude79@comcast.net>
2017-04-03 09:57:21 -0400

    Only one light on at a time with exception of when interval is called

--- stoplight.js
+++ stoplight.js
@@ -3,9 +3,14 @@ let $yellow = $("#yellow");
 let $green = $("#green");
 
 $red.on('click', function() {
+  clearInterval($yellow);
   $red.toggleClass("active");
+  $yellow.removeClass("active");
+  $green.removeClass("active");
 })
 $yellow.on('click', function() {
+  $red.removeClass("active");
+  $green.removeClass("active");
   $yellow.toggleClass("active");
   setInterval(function () {
     $red.addClass("active");
@@ -13,5 +18,8 @@ $yellow.on('click', function() {
   }, 2000);
 })
 $green.on('click', function() {
+  clearInterval($yellow);
   $green.toggleClass("active");
+  $red.removeClass("active");
+  $yellow.removeClass("active");
 })
