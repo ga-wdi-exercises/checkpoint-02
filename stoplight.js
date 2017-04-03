d3d8621fef9e2fcc96b4496e54d7c6e7c8d1d45c
Jonathan Wright <jonwright.va@gmail.com>
2017-04-03 09:48:51 -0400

    JavaScript Part 2 - Question 6

--- stoplight.js
+++ stoplight.js
@@ -1,15 +1,17 @@
 $('#red').click(() => {
+  $('.bulb').removeClass('active');
   $('#red').toggleClass( 'active');
 });
 
 $('#yellow').click(() => {
+  $('.bulb').removeClass('active');
   $('#yellow').addClass( 'active')
-  setInterval(() => {
+  let lightTimer = setTimeout(() => {
     $('#yellow').removeClass( 'active');
     $('#red').addClass( 'active');},2000);
-
 });
 
 $('#green').click(() => {
+  $('.bulb').removeClass('active');
   $('#green').toggleClass( 'active');
 });
