483724b615724eb5a944da9c35bd13168247308c
Paul Christian-Brown <pchristianbrown@gmail.com>
2017-04-03 10:08:02 -0400

    Multiple answers

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 // Create an array called `colors` that contains three colors.
-
+var colors = ["red", "white", "blue"]{
+}
 // Log the last item in the array to the console.
-
+console.log
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+colors.forEach(function(colors) {
+  console.log("My favorite color is []" + colors)
+});
+}
 // Create an `instructor` object that contains three key-value pairs.
+var= instructor ['height', 'class', 'period']{
 
+}
 // Add a `facial-hair` property to `instructor`.
+let instructor= {'facial-hair']
 
+}
 // Log the value of the `facial-hair` property to the console.
+console.log (instructor);
