72863d198890223f1509e95b9896a8d6a1e31e92
Halimatu Bakarr <halimatu.j.bakarr@gmail.com>
2017-04-03 09:37:14 -0400

    create an click event listener that changes yellow to red after two seconds

--- stoplight.js
+++ stoplight.js
@@ -1,5 +1,15 @@
 $(document).ready(function () {
+  //Red Light
   $("#red").on('click', function(){
     $("#red").toggleClass('active');
   })
+  //Yellow light
+  $("#yellow").on('click', function(){
+    $("#yellow").toggleClass('active');
+    setInterval(function(){
+      $("#red").addClass('active');
+      $("#yellow").removeClass('active');
+    }, 2000);
+  })
+
 });
