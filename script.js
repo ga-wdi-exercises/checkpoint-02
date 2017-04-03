d589ee66a74db290458109e958bdefa132ab042c
Conner Johnson <Conner.johnson33@gmail.com>
2017-04-03 09:37:00 -0400

    section 1 complete

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Create an array called `colors` that contains three colors.
+// Ceate an array called `colors` that contains three colors.
 
 // Log the last item in the array to the console.
 
@@ -9,3 +9,26 @@
 // Add a `facial-hair` property to `instructor`.
 
 // Log the value of the `facial-hair` property to the console.
+
+
+let colors= ["red", "yellow", "green"]
+
+
+let green = colors[2]
+
+for (index = 0; index < colors.length; ++index){
+  console.log("my favorite color is")
+}
+// colors.forEach(fucntion(find)){
+  //console.log("my favorite color is .find")
+    //not so sure if this is correct
+
+let instructor = {
+  gender: male;
+  firstname: john;
+  hair: long;
+}
+
+instructor.facialhair = "got some"
+
+console.log(facialhair)
