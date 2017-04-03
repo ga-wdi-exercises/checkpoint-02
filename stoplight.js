4bbb8434aa4cb716b7fefcb18f4c1f0a1aa978cf
Curtis Ostle <curtisostle@gmail.com>
2017-04-03 10:17:27 -0400

    finsihed all

--- stoplight.js
+++ stoplight.js
@@ -2,27 +2,52 @@ var redlight = $('#red')
 var yellowlight = $('#yellow');
 var greenlight = $('#green');
 
-turnRed();
+var lights = [redlight, yellowlight, greenlight];
+
+
+redlight.toggleClass('active');
 
 redlight.on('click', () => {redlight.toggleClass('active')});
 yellowlight.on('click', turnYellow);
 greenlight.on('click',turnGreen);
 
 function turnGreen() {
-  greenlight.toggleClass('active');
+  if(!(checkLights())) {
+      greenlight.toggleClass('active');
+    };
 }
 
 function turnYellow() {
-  yellowlight.toggleClass('active');
-  setTimeout(yellowToRed,2000);
+  if(!(checkLights())) {
+    yellowlight.toggleClass('active');
+    setTimeout(yellowToRed,2000);
+  }
 }
 
 
 function yellowToRed() {
-  yellowlight.toggleClass('active');
-  turnRed();
-}
+    yellowlight.toggleClass('active');
+    turnRed();
+  }
+
 
 function turnRed() {
-  redlight.toggleClass('active');
+  if(!(checkLights())) {
+    redlight.toggleClass('active');
+  }
 }
+
+
+
+function checkLights () {
+  for(var i = 0 ;i < lights.length; i++) {
+    if(lights[i].hasClass('active')) {
+      return true;
+    };
+  };
+}
+//   lights.every(function(light){
+//     if(!(light.hasClass('active'))) {
+//     };
+//   });
+// }
