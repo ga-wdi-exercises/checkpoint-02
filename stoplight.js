8594528c38c77593614bee9b125dfbc09d1590f3
Conner Johnson <Conner.johnson33@gmail.com>
2017-04-03 09:58:36 -0400

    moved items to stoplight.js

---
+++ stoplight.js
@@ -0,0 +1,33 @@
+let colors= ["red", "yellow", "green"]
+
+
+let green = colors[2]
+
+for (i = 0; index < colors.length; ++index){
+  console.log("my favorite color is " + colors[i])
+}
+// colors.forEach(fucntion(find)){
+  //console.log("my favorite color is .find")
+    //not so sure if this is correct
+
+let instructor = {
+  gender: male;
+  firstname: john;
+  hair: long;
+}
+
+instructor.facialhair = "got some"
+
+console.log(facialhair)
+
+$("#red").click(function()){
+  $("#red").toggleClass("active");
+}
+
+$("#yellow").click(function()){
+  $("#yellow").toggleClass("active");
+}
+
+$("#green").click(function()){
+  $("yellow").toggleClass("active");
+}
