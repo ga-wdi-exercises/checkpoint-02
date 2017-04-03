41f591791e98c3954e065e0b574e3c810128874f
Marcus Linn Miller Jr <marcusmiller220@gmail.com>
2017-04-03 10:10:52 -0400

    java2 question4

--- script.js
+++ script.js
@@ -1,11 +1,22 @@
+var colors = ["blue", "red", "yellow"];
 // Create an array called `colors` that contains three colors.
-
+console.log(colors[2]);
 // Log the last item in the array to the console.
+var colors = ["blue," "red", "yellow"];
+var arrayLength = colors.length;
+var favorite ="My favorite color is"
+for(var x = -1; x <= colors.length; x++) {
+    console.log(favorite.colors[x]);
+}
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+var instructor("first-name", "height", "hair-length") = {
+    first-name = "first-name",
+    height = "height",
+    hair-length - "hair-length"
+}
 // Create an `instructor` object that contains three key-value pairs.
-
+instructor.facialhair = "facial-hair"
 // Add a `facial-hair` property to `instructor`.
 
 // Log the value of the `facial-hair` property to the console.
