c6c063075147ceef98908aae5c8404e08625f603
Stormy Ramsey <stormy.ramsey@gmail.com>
2017-04-03 09:58:14 -0400

    object added

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
-// Create an array called `colors` that contains three colors.
-var colors = {'green', 'yellow', 'red'};
+var colors = ['green', 'yellow', 'red'];
 
 // Log the last item in the array to the console.
 
-   console.log(colors[2]); 
+   console.log(colors[2]);
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+for(i = 0; i < colors.length; i++){
+  var text = "";
+  text += "My favoriate color is" + " " + colors[i];
+}
 // Create an `instructor` object that contains three key-value pairs.
+var instructor = {
+name: "Andy",
+program: "WDI",
+gender:"male"
+};
 
 // Add a `facial-hair` property to `instructor`.
-
+instructor.appearance = "facial-hair"
 // Log the value of the `facial-hair` property to the console.
+console.log(instructor.appearance);
