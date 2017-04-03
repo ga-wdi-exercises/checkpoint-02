c0e8eb971be5a04cf942a804147db45a088b2e49
Whitney <whitney.purdum@gmail.com>
2017-04-03 09:27:13 -0400

    add instructor object

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 // Create an array called `colors` that contains three colors.
 
-var colors = ['red', 'green', 'yellow']
+var colors = ['red', 'green', 'yellow'];
+var instructor = {
+	name: "Whitney",
+	expertise: "CSS",
+	location: "Washington, D.C.",
+};
 
 // Log the last item in the array to the console.
 
 console.log(colors[2]);
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+
 for (var i = 0; i < colors.length; i++) {
 	console.log("My favorite color is [i]");
 };
