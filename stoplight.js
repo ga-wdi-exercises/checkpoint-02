b6ec2cf2528794e324bc79f67cd8bae01954248e
Omar Chaudhry <omarchaudhry.1991@gmail.com>
2017-04-03 10:38:46 -0400

    123

--- stoplight.js
+++ stoplight.js
@@ -1,2 +1,24 @@
-$( "changeTheLight" ).on( "click", .red ).on( "click", .yellow ).on( "click", .green );
-$( "p" ).off( "click", bar );
+document.getElementById('redButton').addEventListener("click", turnItRed);
+document.getElementById('yellowButton').addEventListener("click", turnItYellow);
+document.getElementById('greenButton').addEventListener("click", turnItGreen);
+
+
+function turnItRed() {
+  document.getElementById(.active.red).style.backgroundColor = "red";
+  document.getElementById(active.yellow).style.backgroundColor = "black";
+  document.getElementById(active.green).style.backgroundColor = "black";
+}
+
+function turnItYellow() {
+  clearLights();
+  document.getElementById(.active.red).style.backgroundColor = "black";
+  document.getElementById(active.yellow).style.backgroundColor = "yellow";
+  document.getElementById(active.green).style.backgroundColor = "black";
+  moment.duration(500)
+}
+
+function turnItGreen() {
+  document.getElementById(.active.red).style.backgroundColor = "red";
+  document.getElementById(active.yellow).style.backgroundColor = "black";
+  document.getElementById(active.green).style.backgroundColor = "green";
+}
