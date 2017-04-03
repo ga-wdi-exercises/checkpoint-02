2a4cfbb9816dd44d60b595a3313a767421ec53f8
Adam Hoff <swimmerdude79@comcast.net>
2017-04-03 09:41:15 -0400

    Created event listeners for light functionality

---
+++ stoplight.js
@@ -0,0 +1,13 @@
+let $red = $("#red");
+let $yellow = $("#yellow");
+let $green = $("#green");
+
+$red.on('click', function() {
+  $red.addClass("active");
+})
+$yellow.on('click', function() {
+  $yellow.addClass("active");
+})
+$green.on('click', function() {
+  $green.addClass("active");
+})
