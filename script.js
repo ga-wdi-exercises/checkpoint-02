51ced53f35aa9435d1abeade1a4b0463674e2239
Jonathan Wright <jonwright.va@gmail.com>
2017-04-03 09:29:41 -0400

    JavaScript Part 1 complete

--- script.js
+++ script.js
@@ -1,14 +1,22 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+
+  // Create an array called `colors` that contains three colors.
+  let colors = ['red', 'blue', 'salmon'];
+  // Log the last item in the array to the console.
+  console.log(colors[2]);
+  // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+  colors.forEach((color) => {
+    console.log(`My favorite color is ${color}`);
+  })
+  // Create an `instructor` object that contains three key-value pairs.
+  let instructor = {
+    firstName: 'Vick',
+    lastName: 'Vinegar',
+    age: 34
+  };
+  // Add a `facial-hair` property to `instructor`.
+  instructor['facial-hair'] = 'None'
+  // Log the value of the `facial-hair` property to the console.
+  console.log(instructor['facial-hair']);
+  
 });
-// Create an array called `colors` that contains three colors.
-
-// Log the last item in the array to the console.
-
-// Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
-// Create an `instructor` object that contains three key-value pairs.
-
-// Add a `facial-hair` property to `instructor`.
-
-// Log the value of the `facial-hair` property to the console.
