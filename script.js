6e3058dce305e5363aea1e5e0efdc740692f6da5
Adam Hoff <swimmerdude79@comcast.net>
2017-04-03 09:24:23 -0400

    created loop that prints my favorite colors

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-var colors = ["red", "blue", "green"];
-console.log(colors[2]);
-
 // Create an array called `colors` that contains three colors.
-
+var colors = ["red", "blue", "green"];
 // Log the last item in the array to the console.
-
+console.log(colors[2]);
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
-
+colors.forEach(function(item) {
+  console.log("My favorite color is " + item);
+})
 // Create an `instructor` object that contains three key-value pairs.
 
 // Add a `facial-hair` property to `instructor`.
