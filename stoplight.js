e9583c93d5f63acb4b11760a761571e52be0f691
Tim Chase <tchase4544@gmail.com>
2017-05-15 09:50:08 -0400

    light toggles buggy

--- stoplight.js
+++ stoplight.js
@@ -1,16 +1,35 @@
 $(document).ready(function(){
+	let bulbs = $('.bulb');
 
-	$('.bulb').on("click",function(){
+	//click bulb turns on
+	bulbs.on("click",function(){
+		let that = $(this)
 
-		$(this).toggleClass("active")
+		bulbs.removeClass('active')
+
+		that.addClass('active')
+		
+		// very buggie toggle features
+		// if( that.hasClass('active') ){
+
+		// 	that.removeClass('active')
+
+		// }
+
+		// $(this).toggleClass("active")
 
 	})
+
+	//yellow click event
 	$('#yellow').on("click",(function(){
 
 		setTimeout(lightTimer, 2000)
 
 	}))
+
+	//yellow light timer
 	function lightTimer() {
+		bulbs.removeClass('active')
 		$('#red').addClass('active')
 	}
 
