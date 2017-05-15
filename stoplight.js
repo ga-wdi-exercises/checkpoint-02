ba0f7be6469e5f6389d221182db8f358409f92d4
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-15 09:18:37 -0400

    JS-part 2 #5, complete

--- stoplight.js
+++ stoplight.js
@@ -5,6 +5,15 @@ $(document).ready(function() {
 
   $('#yellow').click(function() {
     $('#yellow').toggleClass("active");
+
+    var changetoRed = setInterval(yellowChangeRed, 2000);
+
+    function yellowChangeRed() {
+      $('#red').toggleClass("active");
+      clearInterval(changetoRed);
+      $('#yellow, #green').removeClass("active");
+    }
+
   })
 
   $('#green').click(function() {
