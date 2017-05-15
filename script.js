18e8aa53f5840fd800be7dcbbf00d40078e2ee4c
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-15 09:08:05 -0400

    JS-part 1b, complete

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
 // Create an array called `colors` that contains three colors.
+let colors = ["red", "green", "blue"]
 
 // Log the last item in the array to the console.
+console.log(colors[2]);
 
 // Create a `for` loop that prints out "My favorite color is [insert color here]" for each item in `colors`.
+for (var i = 0; i < colors.length; i++) {
+  console.log(`My favorite color is ${colors[i]}`);
+}
+
 
 // Create an `instructor` object that contains three key-value pairs.
+let instructor = {
+  name: "Sonny",
+  age: 25,
+  gender: "female"
+}
 
 // Add a `facial-hair` property to `instructor`.
+instructor["facial-hair"] = "none";
 
 // Log the value of the `facial-hair` property to the console.
+console.log(instructor["facial-hair"]);
