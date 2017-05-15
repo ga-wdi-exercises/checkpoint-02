9c402c0851d1bf4232b2956c31ad07c9e95dab58
Bao Cong Pham <BaoPham92@yahoo.com>
2017-05-15 09:52:03 -0400

    please look at my Jquery

--- script.js
+++ script.js
@@ -9,3 +9,20 @@
 // Add a `facial-hair` property to `instructor`.
 
 // Log the value of the `facial-hair` property to the console.
+
+var colors = ["red", "green", "orange"];
+
+console.log(colors[2]);
+
+for(var index in colors){
+	console.log("My favorite colors is " + colors[index]);
+}
+
+var instructor = {
+	switch1: "switch next color",
+	switch2: "switch last color",
+	switch3: "turn off power"
+};
+instructor.facialHair = "five-0-clock shadow";
+	console.log("James actually trimmed his beard to a " + instructor.facialHair + " \"Boolean = false\"")
+//End of above directions.
