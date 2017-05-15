646786d0a0a91d41369ab92fa763ce1a8c105170
SONNY KEOBOUNPHANH <SKEOBOUNPHANH@GMAIL.COM>
2017-05-15 09:16:27 -0400

    JS-part 2 #3-4, complete

--- stoplight.js
+++ stoplight.js
@@ -1,1 +1,13 @@
+$(document).ready(function() {
+  $('#red').click(function() {
+    $('#red').toggleClass("active");
+  })
 
+  $('#yellow').click(function() {
+    $('#yellow').toggleClass("active");
+  })
+
+  $('#green').click(function() {
+    $('#green').toggleClass("active");
+  })
+})
