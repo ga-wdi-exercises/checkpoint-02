d635af02bff1bea97d458bc69332f60bdb865048
tedlayea <tedlayea@gMAIL.com>
2017-05-15 09:51:54 -0400

    part 2 Q3 and Q4

---
+++ stoplight.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+var redButton=$("")
+var greenButton=$("")
+var yellowButton=$("")
+
+
+function stopFunction(){
+$("#red").toggleclss(.active#red)
+
+}
+function goFunction(){
+
+  $("#green").toggleclass(.active#green)
+}
+function warningFunction(){
+$("#yellow").toggleclass(.active#yellow)
+}
+
+redButton.on("click",stopFunction)
+greenButton.on("click",goFunction)
+yellowButton.on("click",warningFunction)
+
+
+
+}
